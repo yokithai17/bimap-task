834a9b8d9422ae6aa3596553e7a3d1f03b746c1e
Artem Koton <kotonartem@protonmail.com>
2023-09-24 03:47:51 +0300

    Bump template repository

--- ci-extra/test-valgrind.sh
+++ ci-extra/test-valgrind.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 IFS=$' \t\n'
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 valgrind --tool=memcheck \
   --gen-suppressions=all \
--- test/test-classes.h
+++ test/test-classes.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "gtest/gtest.h"
+#include <gtest/gtest.h>
 
 #include <cmath>
 #include <unordered_set>
